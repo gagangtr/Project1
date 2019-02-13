796f9b39a3761a261932b07d0503ffbd6a0087ba
A Gagantej <31537812+gagangtr@users.noreply.github.com>
2019-02-13 19:29:23 +0530

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -2,5 +2,6 @@
 pwd
 sudo cp -p /var/lib/jenkins/workspace/Build1/ABC.exe /home/ec2-user/jenkinsBackup/
 cd /home/ec2-user/jenkinsBackup/
-pwd
+date=date +'%d/%m/%Y_%H:%M:%S'
+mv ABC.exe ABC_$date.exe
 ls
